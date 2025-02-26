9933f34dcbdd74c2b3fd7a836bb3ab4736ea491b
satvik nanda <satvik.nanda@gmail.com>
2025-02-26 23:04:17 +0530

    cuda not available

--- testing.py
+++ testing.py
@@ -1,3 +1,11 @@
 # this is just a rough python script for my testing 
 
 import lzma
+from torch import cuda
+
+if cuda.is_available():
+    print("yes")
+else:
+    print("no")
+
+#since my laptop has intel it does not support cuda
